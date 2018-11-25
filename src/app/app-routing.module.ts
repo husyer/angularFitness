1a0688c711c20b64b866c8516cfea5cca24109dc
dorian <dorian.dauphin@gmail.com>
2018-11-25 18:31:57 +0100

    adding training routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
-  { path: '**', component:  LoginComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent },
+  { path: '**', component: WelcomeComponent },
 ];
 
 @NgModule({
